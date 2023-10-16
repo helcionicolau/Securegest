f6a95273533989a835b38e6c9f26e7fe00af2467
Helcio Nicolau <104235693+helcionicolau@users.noreply.github.com>
2023-10-17 00:56:16 +0100

    Update sequelize.js
    Alterações no sequilize conection...

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -1,43 +1,31 @@
-const sequelize = require("sequelize");
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const dontenv = require("dotenv");
-dontenv.config();
-
-
+// Obtendo informações do banco de dados a partir de variáveis de ambiente
 const DATABASE = process.env.MYSQL_DATABASE || "u102941870_securegest_db";
 const USERNAME = process.env.MYSQL_USER || "u102941870_securegest";
 const PASSWORD = process.env.MYSQL_PASSWORD || "ny@oRW!c8xV/";
-const CONFIG = {
+const PORT = process.env.MYSQL_PORT || "3306";
+const HOST = process.env.MYSQL_HOST || "localhost";
+
+const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
   dialect: "mysql",
-  port: process.env.MYSQL_PORT || "3306",
-  host: process.env.MYSQL_HOST || "localhost",
+  host: HOST,
+  port: PORT,
   define: {
     timestamps: false,
-  }
-}
-
-let db = {};
-
-try {
-  db = new sequelize(DATABASE, USERNAME, PASSWORD, CONFIG);
-} catch (error) {
-  console.error("Erro na conxeão com o banco de dados." + error);
-}
-
-async function conexaoautenticao() {
-
-  try {
-    await db.authenticate();
-    console.log("Conectado com sucesso ao banco de dados...");
-  } catch (error) {
-    console.error("Erro na conxeão com o banco de dados." + error);
-  }
-}
-
-Object.assign(db, {
-  conexaoautenticao
+  },
 });
 
-module.exports = db;
+async function conexaoautenticacao() {
+  try {
+    await sequelize.authenticate();
+    console.log("Conectado com sucesso ao banco de dados...");
+  } catch (error) {
+    console.error("Erro na conexão com o banco de dados: " + error);
+  }
+}
 
-// Created by António Baptista #(24/08/2023)
+// Exporte o objeto Sequelize configurado e a função de autenticação
+module.exports = { sequelize, conexaoautenticacao };
