10444e74fe3140594564a49fde5db8b58656c2dd
Helcio Nicolau <104235693+helcionicolau@users.noreply.github.com>
2023-10-17 01:26:00 +0100

    Update server.js

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,13 +1,6 @@
 // Importando o módulo 'app'
 const app = require('../app');
 
-// Criacao do Server para o APP
-const server = http.createServer(app);
-// Porta para o Server escutar
-server.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}\n// Created by António Baptista #(24/08/2023)`);
-});
-
 // Função para lidar com as solicitações HTTP
 exports.handler = async (event) => {
   // Evento contém informações sobre a solicitação HTTP recebida
