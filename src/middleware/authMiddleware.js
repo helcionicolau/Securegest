9fb2f876e5f8261a8ba71d317c606e7236ec89a1
Helcio Nicolau <helcio05nicolau@outlook.com>
2023-11-21 19:26:09 +0100

    Alterando o AuthMidd...

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = {
-  authenticateMiddleware: (scope) => (req, res, next) => {
+  authenticateMiddleware: () => (req, res, next) => {
     try {
       const token = req.headers.authorization.split(' ')[1];
       if (!token) {
