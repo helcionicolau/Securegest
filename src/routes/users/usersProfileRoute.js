7701ef1ab5d136a3c5c58de864c198c8929280fd
Helcio Nicolau <helcio05nicolau@outlook.com>
2023-11-29 21:40:51 +0100

    Bug na rota - perfis_usuario

--- src/routes/users/usersProfileRoute.js
+++ src/routes/users/usersProfileRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userProfileController = require('../../controllers/user//userProfileController');
+const userProfileController = require('../../controllers/user/userProfileController');
 const authMiddleware = require('../../middleware/authMiddleware');
 
 
