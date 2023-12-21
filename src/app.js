c6d77883324b67b9ca2850ffd73c1dd196050a5d
Helcio Nicolau <helcio05nicolau@outlook.com>
2023-12-21 17:27:33 +0100

    Eliminação de comentários no arquivo app.js

--- src/app.js
+++ src/app.js
@@ -9,26 +9,19 @@ const app = express();
 
 db.conexaoautenticao();
 
-// Middleware para processar dados do corpo das requisições
-app.use(bodyParser.urlencoded({ extended: false })); // Dados simples
-app.use(bodyParser.json()); // Formato JSON
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Middleware para lidar com CORS (Cross-Origin Resource Sharing)
 app.use(cors());
 
-// Usar as rotas definidas no arquivo index.js
 app.use(
   express.json(), routes, express.urlencoded({ extended: false })
 );
 
-// Monitora toda a execução
 app.use(morgan('dev'));
 
-// CORS
-// Servidores acessíveis para a API, nesse caso @Todos 
-// Métodos que serão manipulados por esse servidor que vai acessar a API
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Todos os endereços de servidores
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
@@ -41,12 +34,10 @@ app.use((req, res, next) => {
 });
 
 
-// Lidar com rotas não encontradas (404)
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Rota não encontrada' });
 });
 
-// Lidar com erros internos do servidor (500)
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ error: 'Erro interno do servidor' });
@@ -58,6 +49,5 @@ app.listen(port, () => {
   console.log(`Servidor rodando em http://mysql-securegestdb.alwaysdata.net:${port}\n// Created by António Baptista #(24/08/2023)`);
 })
 
-// module.exports = app;
 
 // Created by António Baptista #(24/08/2023)
