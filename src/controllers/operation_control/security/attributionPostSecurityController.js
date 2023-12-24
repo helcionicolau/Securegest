9e80dfe07be27105f2612eb965c6cadb86cf1135
Helcio Nicolau <helcio05nicolau@outlook.com>
2023-12-24 01:10:53 +0100

    ...

--- src/controllers/operation_control/security/attributionPostSecurityController.js
+++ src/controllers/operation_control/security/attributionPostSecurityController.js
@@ -57,7 +57,6 @@ module.exports = {
                 return res.status(403).json({ error: 'Usuário não autorizado a adicionar seguranças' });
             }
 
-            const id_posto = req.body.id_posto; // Certifique-se de que o campo está sendo enviado no corpo da requisição
             if (!id_posto) {
                 return res.status(400).json({ error: 'O campo id_posto é obrigatório' });
             }
