afd28e5ceeeccf61d4ba1bbab5db371b977b7060
António Baptista <antonio.baptista@afrizona.co.ao>
2024-04-08 20:31:59 +0100

    Update nas rotas

--- src/routes/role_access/roleAccessRoute.js
+++ src/routes/role_access/roleAccessRoute.js
@@ -4,7 +4,7 @@ const roleAccessController = require('../../controllers/role_access/roleAccessCo
 const authMiddleware = require('../../middleware/authMiddleware');
 
 // Rotas para o CRUD de role access
-router.post('/', authMiddleware.authenticateUserMiddleware, roleAccessController.createRoleAccess);
+router.post('/register', authMiddleware.authenticateUserMiddleware, roleAccessController.createRoleAccess);
 router.get('/', authMiddleware.authenticateUserMiddleware, roleAccessController.getAllRoleAccess);
 router.get('/:roleAccessId', authMiddleware.authenticateUserMiddleware, roleAccessController.getRoleAccessById);
 router.put('/:roleAccessId', authMiddleware.authenticateUserMiddleware, roleAccessController.updateRoleAccess);
