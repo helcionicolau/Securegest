ddb5add656f8ca4e071a847bfc30b365a7e0ee13
António Baptista <antonio.baptista@afrizona.co.ao>
2024-04-10 00:12:42 +0100

    Voltando no Sequelize

--- src/models/roleaccess/Roleaccess.js
+++ src/models/roleaccess/Roleaccess.js
@@ -1,17 +1,58 @@
-// Roleaccess.js
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../../utils/sequelize");
 
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const RoleAccess = sequelize.define('role_access', {
+  id_rm: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    field: "id_rm"
+  },
+  role_id: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    field: "role_id"
+  },
+  menu_id: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    field: "menu_id"
+  },
+  haveedit: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: false,
+    field: "haveedit"
+  },
+  haveadd: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: false,
+    field: "haveadd"
+  },
+  havedelete: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: false,
+    field: "havedelete"
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+    field: 'created_at'
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+    field: 'updated_at'
+  }
+}, {
+  tableName: "role_access",
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
 
-const RoleAccess = prisma.roleAccess;
-const Role = prisma.role;
-const Menu = prisma.menu;
-
-RoleAccess.belongsTo(Role, { foreignKey: 'role_id' });
-RoleAccess.belongsTo(Menu, { foreignKey: 'menu_id' });
-
-module.exports = {
-  RoleAccess,
-  Role,
-  Menu,
-};
