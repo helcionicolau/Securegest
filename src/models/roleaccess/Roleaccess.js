f25c469bd85822af999bbaff127b3f19a32868f2
António Baptista <antonio.baptista@afrizona.co.ao>
2024-04-09 23:31:45 +0100

    RoleAccess

--- src/models/roleaccess/Roleaccess.js
+++ src/models/roleaccess/Roleaccess.js
@@ -1,62 +1,6 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../../utils/sequelize");
 
-const RoleAccess = sequelize.define('role_access', {
-  id_rm: {
-    type: DataTypes.BIGINT,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false,
-    field: "id_rm"
-  },
-  role_id: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-    field: "role_id",
-    references: {model: Role}
-  },
-  menu_id: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-    field: "menu_id",
-    references: {model: Menu}
-  },
-  haveedit: {
-    type: DataTypes.TINYINT,
-    allowNull: false,
-    defaultValue: false,
-    field: "haveedit"
-  },
-  haveadd: {
-    type: DataTypes.TINYINT,
-    allowNull: false,
-    defaultValue: false,
-    field: "haveadd"
-  },
-  havedelete: {
-    type: DataTypes.TINYINT,
-    allowNull: false,
-    defaultValue: false,
-    field: "havedelete"
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-    field: 'created_at'
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-    field: 'updated_at'
-  }
-}, {
-  tableName: "role_access",
-  timestamps: true,
-  createdAt: 'created_at',
-  updatedAt: 'updated_at'
-});
 
 const Role = sequelize.define('roles', {
   id_role: {
@@ -115,6 +59,63 @@ const Menu = sequelize.define('menus', {
   }
 }, {
   tableName: "menus"
+});
+
+const RoleAccess = sequelize.define('role_access', {
+  id_rm: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    field: "id_rm"
+  },
+  role_id: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    field: "role_id",
+    references: {model: Role}
+  },
+  menu_id: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    field: "menu_id",
+    references: {model: Menu}
+  },
+  haveedit: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: false,
+    field: "haveedit"
+  },
+  haveadd: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: false,
+    field: "haveadd"
+  },
+  havedelete: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: false,
+    field: "havedelete"
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+    field: 'created_at'
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+    field: 'updated_at'
+  }
+}, {
+  tableName: "role_access",
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
 });
 
 
