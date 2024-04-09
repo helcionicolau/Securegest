518820f5207953efe551d5885ac8f365d616574c
António Baptista <antonio.baptista@afrizona.co.ao>
2024-04-10 00:09:14 +0100

    Prisma.v1 - RoleAccess

--- src/models/roleaccess/Roleaccess.js
+++ src/models/roleaccess/Roleaccess.js
@@ -1,129 +1,17 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = require("../../utils/sequelize");
+// Roleaccess.js
 
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-const Role = sequelize.define('roles', {
-  id_role: {
-    type: DataTypes.BIGINT,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false,
-    field: "id_role"
-  },
-  nome: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    field: "nome"
-  },
-  descricao: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    field: "descricao"
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-    field: 'created_at'
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-    field: 'updated_at'
-  }
-}, {
-  tableName: "roles",
-  timestamps: true,
-  createdAt: 'created_at',
-  updatedAt: 'updated_at'
-});
+const RoleAccess = prisma.roleAccess;
+const Role = prisma.role;
+const Menu = prisma.menu;
 
-const Menu = sequelize.define('menus', {
-  id_menu: {
-    type: DataTypes.BIGINT,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false,
-    field: "id_menu"
-  },
-  nome: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    field: "nome"
-  },
-  descricao: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-    field: "descricao"
-  }
-}, {
-  tableName: "menus"
-});
-
-const RoleAccess = sequelize.define('role_access', {
-  id_rm: {
-    type: DataTypes.BIGINT,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false,
-    field: "id_rm"
-  },
-  role_id: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-    field: "role_id",
-    references: {model: Role}
-  },
-  menu_id: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-    field: "menu_id",
-    references: {model: Menu}
-  },
-  haveedit: {
-    type: DataTypes.TINYINT,
-    allowNull: false,
-    defaultValue: false,
-    field: "haveedit"
-  },
-  haveadd: {
-    type: DataTypes.TINYINT,
-    allowNull: false,
-    defaultValue: false,
-    field: "haveadd"
-  },
-  havedelete: {
-    type: DataTypes.TINYINT,
-    allowNull: false,
-    defaultValue: false,
-    field: "havedelete"
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-    field: 'created_at'
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-    field: 'updated_at'
-  }
-}, {
-  tableName: "role_access",
-  timestamps: true,
-  createdAt: 'created_at',
-  updatedAt: 'updated_at'
-});
-
-
-Role.belongsToMany(Menu, { through: RoleAccess, foreignKey: 'role_id' }); 
-Menu.belongsToMany(Role, { through: RoleAccess, foreignKey: 'menu_id' });
+RoleAccess.belongsTo(Role, { foreignKey: 'role_id' });
+RoleAccess.belongsTo(Menu, { foreignKey: 'menu_id' });
 
 module.exports = {
   RoleAccess,
   Role,
-  Menu
+  Menu,
 };
