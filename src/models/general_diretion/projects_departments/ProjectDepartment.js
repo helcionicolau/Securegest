e01f593a64e0a5ef6d17e484b84928c19fe0bb76
António Baptista <antonio.baptista@afrizona.co.ao>
2024-04-10 22:25:17 +0100

    ProjetoDepartamento Table

--- src/models/general_diretion/projects_departments/ProjectDepartment.js
+++ src/models/general_diretion/projects_departments/ProjectDepartment.js
@@ -59,7 +59,7 @@ const ProjetoDepartamento = sequelize.define('projetos_departamentos', {
 });
 
 // Define many-to-many relationship between Projeto and Departamento through ProjetoDepartamento
-ProjetoDepartamento.belongsTo(Projeto, { foreignKey: 'id_projeto', as: 'projeto' });
-ProjetoDepartamento.belongsTo(Departamento, { foreignKey: 'id_departamento', as: 'departamento' });
+ProjetoDepartamento.belongsToMany(Projeto, { foreignKey: 'id_projeto', as: 'projeto' });
+ProjetoDepartamento.belongsToMany(Departamento, { foreignKey: 'id_departamento', as: 'departamento' });
 
 module.exports = ProjetoDepartamento;
