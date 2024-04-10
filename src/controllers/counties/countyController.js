7fd0aa4a5b5fe83f7b58ee5d8e43ebed4790b0c3
António Baptista <antonio.baptista@afrizona.co.ao>
2024-04-10 11:57:37 +0100

    Provincia & Municipio

--- src/controllers/counties/countyController.js
+++ src/controllers/counties/countyController.js
@@ -1,10 +1,13 @@
 // controllers/MunicipioController.js
-const { countyModel } = require('../../models');
+const { countyModel, provinceModel } = require('../../models');
 
 module.exports = {
   async getAllMunicipios(req, res) {
     try {
-      const municipios = await countyModel.findAll();
+      const municipios = await countyModel.findAll({
+        include: [{ model: provinceModel, as: 'provincia' }]
+      });
+
       res.json(municipios);
     } catch (error) {
       console.error(error);
@@ -16,10 +19,11 @@ module.exports = {
     const municipioId = req.params.municipioId;
 
     try {
-      const municipio = await countyModel.findByPk(municipioId);
+      const municipio = await countyModel.findByPk(municipioId, { include: [{ model: provinceModel, as: 'provincia' }] });
       if (!municipio) {
         return res.status(404).json({ error: 'Município não encontrado' });
       }
+
       res.json(municipio);
     } catch (error) {
       console.error(error);
@@ -31,14 +35,18 @@ module.exports = {
     const municipioName = req.params.municipioName;
 
     try {
-      const municipio = await countyModel.findOne({ where: { name: municipioName } });
+      const municipio = await countyModel.findOne({
+        where: { name: municipioName },
+        include: [{ model: provinceModel, as: 'provincia' }]
+      });
       if (!municipio) {
         return res.status(404).json({ error: 'Município não encontrado' });
       }
+
       res.json(municipio);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Erro ao buscar município por nome' });
     }
-  },
+  }
 };
