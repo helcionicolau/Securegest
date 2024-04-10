1a712d66e1524c84d850dcbdcc87539bb9901e2d
António Baptista <antonio.baptista@afrizona.co.ao>
2024-04-10 12:30:37 +0100

    Controller da Position

--- src/routes/business_diretion/position/positionsRoute.js
+++ src/routes/business_diretion/position/positionsRoute.js
@@ -7,7 +7,6 @@ const authMiddleware = require('../../../middleware/authMiddleware');
 router.post('/register', authMiddleware.authenticateUserMiddleware, positionController.registerPosicao);
 router.get('/', authMiddleware.authenticateUserMiddleware, positionController.getAllPosicoes);
 router.get('/:posicaoId', authMiddleware.authenticateUserMiddleware, positionController.getPosicaoById);
-router.get('/cliente/:clientId', authMiddleware.authenticateUserMiddleware, positionController.getPosicoesByClientId);
 router.put('/:posicaoId', authMiddleware.authenticateUserMiddleware, positionController.updatePosicao);
 router.delete('/:posicaoId', authMiddleware.authenticateUserMiddleware, positionController.deletePosicao);
 
