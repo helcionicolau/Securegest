813cf0c705f36baee1c9a9eafda6caf6e69d36a3
António Baptista <antonio.baptista@afrizona.co.ao>
2024-06-05 08:12:29 +0100

    Ajustes na Contagem de Materiais de Logisticas por Categoria.

--- src/routes/logistic/logisticsRoute.js
+++ src/routes/logistic/logisticsRoute.js
@@ -7,10 +7,9 @@ const authMiddleware = require('../../middleware/authMiddleware');
 // Rotas para o CRUD de logistica
 router.post('/register', authMiddleware.authenticateUserMiddleware, logisticaController.createLogistica);
 router.get('/', authMiddleware.authenticateUserMiddleware, logisticaController.getAllLogisticas);
+router.get('/count-by-category', authMiddleware.authenticateUserMiddleware, logisticaController.getLogisticsCountByCategory);
 router.get('/:logisticaId', authMiddleware.authenticateUserMiddleware, logisticaController.getLogisticaById);
 router.put('/:logisticaId', authMiddleware.authenticateUserMiddleware, logisticaController.updateLogistica);
 router.delete('/:logisticaId', authMiddleware.authenticateUserMiddleware, logisticaController.deleteLogistica);
-
-router.get('/count-by-category', authMiddleware.authenticateUserMiddleware, logisticaController.getLogisticsCountByCategory);
 
 module.exports = router;
