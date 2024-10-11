e7144f57e4f47dd2e6ad70f26367a06bc3c9056b
Developer Mindsoft <antonio.baptista@afrizona.co.ao>
2024-10-11 12:27:06 +0100

    AccessMiddleware nos endpoints do funcionário

--- src/routes/rh/employees/employeesRoute.js
+++ src/routes/rh/employees/employeesRoute.js
@@ -2,20 +2,20 @@ const express = require('express');
 const router = express.Router();
 const funcionarioController = require('../../../controllers/rh/employees/employeeController');
 const authMiddleware = require('../../../middleware/authMiddleware');
+const accessMiddleware = require('../../../middleware/accessMiddleware');
 
 // Rotas para o CRUD de funcionários
-router.post('/register', funcionarioController.registerFuncionario);
-router.get('/', authMiddleware.authenticateUserMiddleware, funcionarioController.getAllFuncionarios);
-router.get('/:funcionarioId', authMiddleware.authenticateUserMiddleware, funcionarioController.getFuncionarioById);
-router.put('/:funcionarioId', authMiddleware.authenticateUserMiddleware, funcionarioController.updateFuncionario);
-router.delete('/:funcionarioId', authMiddleware.authenticateUserMiddleware, funcionarioController.deleteFuncionario);
+router.post('/register', authMiddleware.authenticateUserMiddleware, accessMiddleware('add'), funcionarioController.registerFuncionario);
+router.get('/', authMiddleware.authenticateUserMiddleware, accessMiddleware('view'), funcionarioController.getAllFuncionarios);
+router.get('/:funcionarioId', authMiddleware.authenticateUserMiddleware, accessMiddleware('view'), funcionarioController.getFuncionarioById);
+router.put('/:funcionarioId', authMiddleware.authenticateUserMiddleware, accessMiddleware('update'), funcionarioController.updateFuncionario);
+router.delete('/:funcionarioId', authMiddleware.authenticateUserMiddleware, accessMiddleware('delete'), funcionarioController.deleteFuncionario);
 
 // Adicione a nova rota para obter funcionários por departamento
-router.get('/departamento/:departamentoId', authMiddleware.authenticateUserMiddleware, funcionarioController.getFuncionariosByDepartamentoId);
+router.get('/departamento/:departamentoId', authMiddleware.authenticateUserMiddleware, accessMiddleware('view'), funcionarioController.getFuncionariosByDepartamentoId);
 
 // Nova rota para obter funcionários por cargo
-router.get('/cargo/:cargo', authMiddleware.authenticateUserMiddleware, funcionarioController.getFuncionariosByCargo);
-
+router.get('/cargo/:cargo', authMiddleware.authenticateUserMiddleware, accessMiddleware('view'), funcionarioController.getFuncionariosByCargo);
 
 module.exports = router;
 
