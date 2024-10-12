eef6aef512c0bfff5a30002af5bb0ebd12273cb9
Developer Mindsoft <antonio.baptista@afrizona.co.ao>
2024-10-12 22:35:52 +0100

    Photo Path Correction

--- src/routes/rh/employees/employeesRoute.js
+++ src/routes/rh/employees/employeesRoute.js
@@ -4,11 +4,12 @@ const funcionarioController = require('../../../controllers/rh/employees/employe
 const authMiddleware = require('../../../middleware/authMiddleware');
 const accessMiddleware = require('../../../middleware/accessMiddleware');
 const multer = require('multer');
+const caminho = require('../../../uploads/')
 
 // Configuração do multer para upload de fotos
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, '../../../uploads/');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
